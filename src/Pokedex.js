c562d792e25f793f639727d79e9c7a4508d6bafa
jgc050319 <136496193+jgc050319@users.noreply.github.com>
2024-03-05 13:48:39 -0600

    Add files via upload

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -12,10 +12,10 @@ function Pokedex(props) {
 
   return (
     <div className="Pokedex">
-      <h2 className="Pokedex-title">Pokedex</h2>
+      <h2 className="Pokedex-title"  >Pokedex</h2>
       <div className="Pokedex-cards">
         {props.pokemon.map(p => (
-          <Pokecard 
+          <Pokecard key = {p.id}
             id={p.id}
             name={p.name}
             type={p.type}
